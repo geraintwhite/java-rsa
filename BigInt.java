9b05d757e066c78dd260eb00270d8aff546b9801
Geraint White <geraintwhite@gmail.com>
2015-11-24 13:56:53 +0000

    Improve BigInt.modPow()

--- BigInt.java
+++ BigInt.java
@@ -8,38 +8,25 @@ public class BigInt {
   private static final BigInteger ZERO = new BigInteger("0");
   private static final BigInteger ONE = new BigInteger("1");
 
-  public BigInt(String x) {
-    this.x = new BigInteger(x);
-  }
+  public BigInt(String x) { this.x = new BigInteger(x); }
+  public BigInt(BigInteger x) { this.x = x; }
 
-  private static boolean isZero(BigInteger x) {
-    return x.equals(ZERO);
-  }
+  public boolean isZero() { return this.x.equals(ZERO); }
+  public boolean isOne() { return this.x.equals(ONE); }
+  public int intValue() { return this.x.intValue(); }
 
-  private static boolean isOne(BigInteger x) {
-    return x.equals(ONE);
-  }
+  public BigInt and(BigInt x) { return new BigInt(this.x.and(x.x)); }
+  public BigInt mod(BigInt x) { return new BigInt(this.x.mod(x.x)); }
+  public BigInt subtract(BigInt x) { return new BigInt(this.x.subtract(x.x)); }
+  public BigInt multiply(BigInt x) { return new BigInt(this.x.multiply(x.x)); }
+  public BigInt shiftRight(int x) { return new BigInt(this.x.shiftRight(x)); }
 
-  public int intValue() {
-    return x.intValue();
-  }
-
-  public BigInteger modPow(BigInteger exponent, BigInteger modulus) {
-    if (isOne(modulus)) {
-      return ZERO;
+  public BigInt modPow(BigInt exponent, BigInt modulus) {
+    BigInt c = new BigInt(ONE);
+    while (!exponent.isZero()) {
+      c = this.multiply(c).mod(modulus);
+      exponent = exponent.subtract(new BigInt(ONE));
     }
-
-    BigInteger c = ONE;
-    BigInteger x = this.x;
-    while (!isZero(exponent)) {
-      if (isOne(exponent.and(ONE))) {
-        c = x.multiply(c).mod(modulus);
-      }
-
-      exponent = exponent.shiftRight(1);
-      x = x.multiply(x).mod(modulus);
-    }
-
     return c;
   }
 }
--- tests/BigIntTests.java
+++ tests/BigIntTests.java
@@ -1,6 +1,5 @@
 package Assignment2.tests;
 
-import java.math.BigInteger;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import Assignment2.BigInt;
@@ -9,13 +8,13 @@ public class BigIntTests {
   @Test
   public void modPow() {
     BigInt x = new BigInt("5");
-    assertEquals(8, x.modPow(new BigInteger("3"), new BigInteger("13")).intValue());
+    assertEquals(8, x.modPow(new BigInt("3"), new BigInt("13")).intValue());
   }
 
   public static void main(String[] args) {
     BigInt x = new BigInt("5");
     System.out.println(x.intValue());
-    System.out.println(x.modPow(new BigInteger("3"), new BigInteger("13")).intValue());
-    System.out.println(x.intValue());
+    System.out.println(x.modPow(new BigInt("3"), new BigInt("13")).intValue());
+    System.out.println(x.modPow(new BigInt("13"), new BigInt("20")).intValue());
   }
 }
