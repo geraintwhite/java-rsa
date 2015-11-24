adbf719bb5ba8f83d45d78e2aae2639c1e101aae
Geraint White <geraintwhite@gmail.com>
2015-11-24 10:21:28 +0000

    Complete modPow implementation in BigInt

--- BigInt.java
+++ BigInt.java
@@ -3,12 +3,41 @@ package Assignment2;
 import java.math.BigInteger;
 
 public class BigInt {
-  public static long modPow(long base, long exponent, long modulus) {
-    if (modulus == 1) return 0;
+  private BigInteger x;
 
-    long c = 1;
-    for (long i = 0; i < exponent; i++) {
-      c = c * base % modulus;
+  private static final BigInteger ZERO = new BigInteger("0");
+  private static final BigInteger ONE = new BigInteger("1");
+
+  public BigInt(String x) {
+    this.x = new BigInteger(x);
+  }
+
+  private static boolean isZero(BigInteger x) {
+    return x.equals(ZERO);
+  }
+
+  private static boolean isOne(BigInteger x) {
+    return x.equals(ONE);
+  }
+
+  public int intValue() {
+    return x.intValue();
+  }
+
+  public BigInteger modPow(BigInteger exponent, BigInteger modulus) {
+    if (isOne(modulus)) {
+      return ZERO;
+    }
+
+    BigInteger c = ONE;
+    BigInteger x = this.x;
+    while (!isZero(exponent)) {
+      if (isOne(exponent.and(ONE))) {
+        c = x.multiply(c).mod(modulus);
+      }
+
+      exponent = exponent.shiftRight(1);
+      x = x.multiply(x).mod(modulus);
     }
 
     return c;
--- tests/BigIntTests.java
+++ tests/BigIntTests.java
@@ -1,12 +1,21 @@
 package Assignment2.tests;
 
+import java.math.BigInteger;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import Assignment2.BigInt;
 
 public class BigIntTests {
   @Test
-  public static void modPow() {
-    assertEquals(8, BigInt.modPow(5, 3, 13));
+  public void modPow() {
+    BigInt x = new BigInt("5");
+    assertEquals(8, x.modPow(new BigInteger("3"), new BigInteger("13")).intValue());
+  }
+
+  public static void main(String[] args) {
+    BigInt x = new BigInt("5");
+    System.out.println(x.intValue());
+    System.out.println(x.modPow(new BigInteger("3"), new BigInteger("13")).intValue());
+    System.out.println(x.intValue());
   }
 }
