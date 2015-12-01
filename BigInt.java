c86ad17829e9ee5537b5814961d566e1e3024ff6
Geraint White <geraintwhite@gmail.com>
2015-12-01 09:58:28 +0000

    Add comments to BigInt (change to javadoc later)

--- BigInt.java
+++ BigInt.java
@@ -3,20 +3,29 @@ package Assignment2;
 import java.math.BigInteger;
 import java.util.Random;
 
+/**
+ * BigInt class
+ *
+ * Implements methods of the BigInteger class
+ */
 public class BigInt {
   private BigInteger x;
 
+  /* Constants for common BigInt values */
   public static final BigInt ZERO = new BigInt("0");
   public static final BigInt ONE = new BigInt("1");
 
+  /* BigInt contstructors compatible with BigInteger */
   public BigInt(String x) { this.x = new BigInteger(x); }
   public BigInt(BigInteger x) { this.x = x; }
   public BigInt(int b, Random r) { this.x = new BigInteger(b, r); }
 
+  /* BigInt helper methods pass through to BigInteger methods */
   public boolean isZero() { return this.x.equals(ZERO.x); }
   public boolean isOne() { return this.x.equals(ONE.x); }
   public int intValue() { return this.x.intValue(); }
 
+  /* Simple arithmetic methods pass through to BigInteger methods */
   public BigInt and(BigInt x) { return new BigInt(this.x.and(x.x)); }
   public BigInt mod(BigInt x) { return new BigInt(this.x.mod(x.x)); }
   public BigInt add(BigInt x) { return new BigInt(this.x.add(x.x)); }
