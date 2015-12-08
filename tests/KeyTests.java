d2ecd78aaa54793f1174ded963e6efd012efda3a
Geraint White <geraintwhite@gmail.com>
2015-12-08 11:38:31 +0000

    Use proper primes for KeyTests

--- tests/KeyTests.java
+++ tests/KeyTests.java
@@ -8,8 +8,8 @@ import Assignment2.BigInt;
 public class KeyTests {
   @Test
   public void initKey() {
-    Key key = new Key(new BigInt("1337"), new BigInt("8080"));
-    assertEquals(1337, key.exponent.intValue());
-    assertEquals(8080, key.modulus.intValue());
+    Key key = new Key(new BigInt("7"), new BigInt("33"));
+    assertEquals(7, key.exponent.intValue());
+    assertEquals(33, key.modulus.intValue());
   }
 }
