920db40acc938fa28101a9d297e89405ee61e42c
Geraint White <geraintwhite@gmail.com>
2015-12-09 21:59:54 +0000

    Use key objects in RSATests

--- tests/RSATests.java
+++ tests/RSATests.java
@@ -7,6 +7,8 @@ import Assignment2.Key;
 import Assignment2.BigInt;
 
 public class RSATests {
+  private static String plaintext = "The quick brown fox jumps over the lazy dog";
+
   private static BigInt e = new BigInt("54268981190359933835892413");
   private static BigInt d = new BigInt("9891450472976932552041561437604852711016103245476516320999909659153477903604590986948317037032965281929");
   private static BigInt n = new BigInt("18337099292535342907505870568603172004435217363747025576600880443733085345221229897433481398042413370049");
@@ -16,17 +18,17 @@ public class RSATests {
 
   @Test
   public void getPublicKey() {
-    RSA rsa = new RSA(new Key(new BigInt("7"), new BigInt("33")));
+    RSA rsa = new RSA(publicKey);
     Key key = rsa.getPublicKey();
-    assertEquals(7, key.exponent.intValue());
-    assertEquals(33, key.modulus.intValue());
+    assertEquals(e, key.exponent);
+    assertEquals(n, key.modulus);
   }
 
   @Test
   public void encrypt() {
     RSA rsa = new RSA(publicKey);
+    String ciphertext = "5061796068475328011788717651058513190497549134858034282832411873059321079498213788579014387802345422335";
+    assertEquals(ciphertext, rsa.encrypt(plaintext));
 
-    String ciphertext = rsa.encrypt("The quick brown fox jumps over the lazy dog");
-    assertEquals("5061796068475328011788717651058513190497549134858034282832411873059321079498213788579014387802345422335", ciphertext);
   }
 }
