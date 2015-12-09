31a26358ace53945b426b85cc2a337b179dcc271
Geraint White <geraintwhite@gmail.com>
2015-12-09 21:42:45 +0000

    Add public and private key to RSATests

--- tests/RSATests.java
+++ tests/RSATests.java
@@ -7,6 +7,13 @@ import Assignment2.Key;
 import Assignment2.BigInt;
 
 public class RSATests {
+  private static BigInt e = new BigInt("54268981190359933835892413");
+  private static BigInt d = new BigInt("9891450472976932552041561437604852711016103245476516320999909659153477903604590986948317037032965281929");
+  private static BigInt n = new BigInt("18337099292535342907505870568603172004435217363747025576600880443733085345221229897433481398042413370049");
+
+  private static Key publicKey = new Key(e, n);
+  private static Key privateKey = new Key(d, n);
+
   @Test
   public void getPublicKey() {
     RSA rsa = new RSA(new Key(new BigInt("7"), new BigInt("33")));
@@ -17,11 +24,9 @@ public class RSATests {
 
   @Test
   public void encrypt() {
-    BigInt e = new BigInt("58526141510869204035840419");
-    BigInt m = new BigInt("15337720597404335770817004203243347680990093413285641791961208026434705304926829890429073136938836703339");
-    RSA rsa = new RSA(new Key(e, m));
+    RSA rsa = new RSA(publicKey);
 
     String ciphertext = rsa.encrypt("The quick brown fox jumps over the lazy dog");
-    assertEquals("13927730793476586151179250538330884705396863722440195585453050622417519852477443521259472374067520832386", ciphertext);
+    assertEquals("5061796068475328011788717651058513190497549134858034282832411873059321079498213788579014387802345422335", ciphertext);
   }
 }
